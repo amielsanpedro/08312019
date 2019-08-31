da5c2bc19ea0481c8f4777a81b84c3fe00649955
amielsanpedro <amieljustin.sanpedro.iics@ust.edu.ph>
2019-08-31 13:15:34 +0800

    Initial commit

---
+++ app/src/main/java/amiel/sanpedro/com/august31_4itb/MainActivity.kt
@@ -0,0 +1,12 @@
+package amiel.sanpedro.com.august31_4itb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
